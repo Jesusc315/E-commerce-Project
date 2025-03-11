a2ddac1c424879c6d82f18d4fe4bd3b589e030b7
Jesusc315 <Jesusc315@nycstudents.net>
2025-03-11 13:18:05 +0000

    connection

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const mongooose = require('mongoose');
-main().catch(err => console.log(err));
+import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import { Server } from 'http';
 
-async function main() {
-    await mongooose.connect("mongodb://localhost:27017/gitsetup");
-    console.log("DB CONNECTED");
+dotenv.config();
+
+const app=express();
+const port=5000;
+app.use(express.static('client'));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log('Connected to MongoDB'))
+.catch((err)=>console.log(err));
+app.use('/api',require('./routes/api'));
+ 
+app.get('*',(req,res)=>{
+    res.sendFile(join(__dirname,'client/index.html'));
+}
+);
+app.listen(port,()=>console.log(`Server is running on port ${port}`));
+process.on("SINGINT", () => {
+    console.log("Shutting down");
+    Server.close(() => {
+        console.log("Server is closed");
+    }
+    );
 }
